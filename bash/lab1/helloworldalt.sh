9cdf6751b629d575a76e695b675c45428eec79cf
Chandler <cdzula1996@gmail.com>
2016-09-15 20:27:45 +0000

    Files added

---
+++ bash/lab1/helloworldalt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script uses a HERE document to display Hello World!
+
+cat <<EOF
+Hello World!
+EOF
---
+++ bash/lab1/myip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Display my public IP address retrieved from icanhazip.com
+
+wget -qO - icanhazip.com
+
+# alternate way to get my ip from icanhazip
+# curl icanhazip.com
